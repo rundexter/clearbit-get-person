69e25e4aae37c902094eff4cf2fb4801a5e0fa02
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-08 16:20:00 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,9 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                email: 'alex@alexmaccaw.com',
+                given_name: 'Alex',
+                family_name: 'MacCaw'
             }
         }
     }
